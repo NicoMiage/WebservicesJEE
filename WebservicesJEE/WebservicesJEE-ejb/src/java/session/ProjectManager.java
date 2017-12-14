8349d72ce9aa6b579a34d91c2bf91bd66c2e0e6b
NicoMiage <nicolas.kircun@gmail.com>
2017-12-14 21:05:00 +0100

    Ajout projet

---
+++ WebservicesJEE/WebservicesJEE-ejb/src/java/session/ProjectManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entities.Project;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author deptinfo
+ */
+@Stateless
+@LocalBean
+public class ProjectManager {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ WebservicesJEE/WebservicesJEE-ejb/src/java/session/StatusManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entities.Status;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Nicolas Kircun
+ */
+@Stateless
+@LocalBean
+public class StatusManager {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
